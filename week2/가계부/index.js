d659139d28f5654612a54d87a87f199569280fe3
Wang HoEun <hoeunwang@gmail.com>
2023-10-27 21:02:15 +0900

    feat: 저장하기, 모달 열기 수정

--- week2/가계부/index.js
+++ week2/가계부/index.js
@@ -70,10 +70,60 @@ function deleteHistoryItem(index) {
 function openModal() {
   const modal = document.getElementById("modal");
   modal.style.display = "block";
+
+  // 수입/지출 버튼에 따라 카테고리를 동적으로 변경
+  const incomeButton = document.getElementById("income-btn");
+  const expenditureButton = document.getElementById("expenditure-btn");
+  const categorySelect = document.getElementById("category");
+
+  incomeButton.addEventListener("click", () => {
+    incomeButton.classList.add("active");
+    expenditureButton.classList.remove("active");
+    // '수입' 카테고리 설정
+    categorySelect.innerHTML = `
+        <option value="월급">월급</option>
+        <option value="용돈">용돈</option>
+      `;
+  });
+
+  expenditureButton.addEventListener("click", () => {
+    expenditureButton.classList.add("active");
+    incomeButton.classList.remove("active");
+    // '지출' 카테고리 설정
+    categorySelect.innerHTML = `
+        <option value="식비">식비</option>
+        <option value="쇼핑">쇼핑</option>
+      `;
+  });
 }
 
 // 모달 닫기
 function closeModal() {
   const modal = document.getElementById("modal");
   modal.style.display = "none";
+}
+function saveNewItem() {
+  const type = document.querySelector(".modal-btn.active").textContent;
+  const category = document.getElementById("category").value;
+  const amount = parseInt(document.getElementById("amount").value, 10);
+  const description = document.getElementById("description").value;
+
+  if (!category || isNaN(amount) || !description) {
+    alert("모든 필드를 작성해주세요.");
+    return;
+  }
+
+  const newItem = {
+    type,
+    category,
+    amount,
+    description,
+  };
+
+  HISTORY_LIST.push(newItem);
+  renderHistoryList(HISTORY_LIST);
+  renderTotalIncomeAndExpense();
+  renderAssets();
+  closeModal();
+  alert("저장되었습니다.");
 }
