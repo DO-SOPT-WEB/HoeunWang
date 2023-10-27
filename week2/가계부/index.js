48c224d8e207289d1bdb6241622b6ea02c4d438d
Wang HoEun <hoeunwang@gmail.com>
2023-10-27 20:40:33 +0900

    feat: 자산 렌더링

--- week2/가계부/index.js
+++ week2/가계부/index.js
@@ -1,4 +1,4 @@
-import { HISTORY_LIST } from "./constants/COST";
+import { HISTORY_LIST, INIT_BALANCE } from "./constants/COST";
 
 window.addEventListener("DOMContentLoaded", () => {
   renderAssets();
@@ -6,3 +6,8 @@ window.addEventListener("DOMContentLoaded", () => {
   renderHistoryList(HISTORY_LIST);
   setupEventListeners();
 });
+
+function renderAssets() {
+  const balanceElement = document.querySelector(".my-cost");
+  balanceElement.textContent = INIT_BALANCE;
+}
