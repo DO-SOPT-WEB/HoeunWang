6299d65d7bfe46e7440304146db7423c2654a9a8
Wang HoEun <hoeunwang@gmail.com>
2023-10-27 20:41:00 +0900

    feat: 내역 리스트 렌더링

--- week2/가계부/index.js
+++ week2/가계부/index.js
@@ -11,3 +11,20 @@ function renderAssets() {
   const balanceElement = document.querySelector(".my-cost");
   balanceElement.textContent = INIT_BALANCE;
 }
+function renderHistoryList(historyList) {
+  const listsWrapper = document.getElementById("lists-wrapper");
+  listsWrapper.innerHTML = "";
+
+  historyList.forEach((item, index) => {
+    const listItem = document.createElement("div");
+    listItem.classList.add("history-item");
+    listItem.innerHTML = `
+        <div class="item-type">${item.type}</div>
+        <div class="item-category">${item.category}</div>
+        <div class="item-amount">${item.amount}원</div>
+        <div class="item-description">${item.description}</div>
+        <button class="delete" data-index="${index}">X</button>
+      `;
+    listsWrapper.appendChild(listItem);
+  });
+}
