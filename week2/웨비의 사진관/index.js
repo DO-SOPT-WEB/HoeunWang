28db4a47f566453335f908f4469cd0d668457d0a
Wang HoEun <hoeunwang@gmail.com>
2023-10-27 18:09:10 +0900

    feat: 이미지 호버 시 설명 나오며 어둡게 처리

---
+++ week2/웨비의 사진관/index.js
@@ -0,0 +1,16 @@
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+
+const imagesWrappers = $$(".images-wrapper"); //하나의 이미지 card
+
+imagesWrappers.forEach((image) => {
+  image.addEventListener("mouseover", () => {
+    const images__description = image.querySelector(".images__description");
+    images__description.classList.add("description-appear");
+  });
+
+  image.addEventListener("mouseout", () => {
+    const images__description = image.querySelector(".images__description");
+    images__description.classList.remove("description-appear");
+  });
+});
