51485d567c5a04d14d1c219d6b2808fe292b6456
Wang HoEun <hoeunwang@gmail.com>
2023-11-02 23:26:04 +0900

    refactor: hover js 코드 삭제. css에 존재함

--- week2/Gallery/index.js
+++ week2/Gallery/index.js
@@ -1,20 +1,6 @@
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
-const imagesWrappers = $$(".images-wrapper"); //하나의 이미지 card
-
-imagesWrappers.forEach((image) => {
-  image.addEventListener("mouseover", () => {
-    const images__description = image.querySelector(".images__description");
-    images__description.classList.add("description-appear");
-  });
-
-  image.addEventListener("mouseout", () => {
-    const images__description = image.querySelector(".images__description");
-    images__description.classList.remove("description-appear");
-  });
-});
-
 const topBtn = $(".top-button");
 const pageHeight = window.innerHeight;
 
