d7062a7c1e89b59032a00bdea066ed3b15c5b99f
Wang HoEun <hoeunwang@gmail.com>
2023-11-10 04:38:56 +0900

    feat: constant 생성 및 선택지 state 생성

--- week3/recommend-system/src/components/Recommend/Article/ViewOption/index.jsx
+++ week3/recommend-system/src/components/Recommend/Article/ViewOption/index.jsx
@@ -1,12 +1,46 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 
+import { useState } from "react";
+import First from "./First";
+import Second from "./Second";
+import Third from "./Third";
+
 function ViewOption({ selectedHandler, startHandler }) {
-  const check = () => {
-    console.log(selectedHandler, startHandler);
+  const [nthChoice, setNthChoice] = useState(1);
+  const [result, setResult] = useState("");
+
+  const handleSelect = (selected) => {
+    setResult((prevResult) => prevResult + selected + ".");
   };
   return (
     <>
-      <div onClick={check()}>선택지</div>
+      {nthChoice === 1 && (
+        <First
+          selectedHandler={selectedHandler}
+          startHandler={startHandler}
+          resultHandler={handleSelect}
+          nthChoiceHandler={(number) => {
+            setNthChoice(number);
+          }}
+        />
+      )}
+      {nthChoice === 2 && (
+        <Second
+          resultHandler={handleSelect}
+          nthChoiceHandler={(number) => {
+            setNthChoice(number);
+          }}
+        />
+      )}
+      {nthChoice === 3 && (
+        <Third
+          resultHandler={handleSelect}
+          nthChoiceHandler={(number) => {
+            setNthChoice(number);
+          }}
+        />
+      )}
     </>
   );
 }
---
+++ week3/recommend-system/src/constants/IceCreamOption.js
@@ -0,0 +1,44 @@
+export const IceCreamOption = {
+  choco: {
+    chrunk: {
+      old: "엄마는 외계인",
+      new: "초코,우리 이제 헤이즐넛",
+    },
+    only: {
+      old: "초콜릿",
+      new: "초콜릿 무스",
+    },
+    other: {
+      old: "초코나무숲",
+      new: "마법사의 비밀 레시피",
+    },
+  },
+  fruit: {
+    chrunk: {
+      old: "체리쥬빌레",
+      new: "디노 젤리",
+    },
+    only: {
+      old: "바닐라",
+      new: "레드 라즈베리 소르베",
+    },
+    other: {
+      old: "레인보우샤배트",
+      new: "핑크 베리 하츄핑",
+    },
+  },
+  others: {
+    chrunk: {
+      old: "민트초콜릿칩",
+      new: "초코야민트해",
+    },
+    only: {
+      old: "그린티",
+      new: "요거트",
+    },
+    other: {
+      old: "슈팅스타",
+      new: "팝핑 스타 라이즈",
+    },
+  },
+};
