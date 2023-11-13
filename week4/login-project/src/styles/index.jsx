cc24d985ff3a4d53a46660912993caff1efe35e0
Wang HoEun <hoeunwang@gmail.com>
2023-11-14 01:49:24 +0900

    style: 초기 파일 구성 및 font와 global스타일 설정

---
+++ week4/login-project/src/styles/index.jsx
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+import { ThemeProvider } from "styled-components/macro";
+import theme from "./theme";
+import GlobalStyles from "./GlobalStyles";
+
+Styles.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default function Styles({ children }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      {children}
+    </ThemeProvider>
+  );
+}
